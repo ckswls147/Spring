0359d0b60bdf531e6eaac72c8d1a8646d7c7429f
ckswls147@naver.com <ckswls147@naver.com>
2022-12-06 13:07:43 +0900

    Learn Servlet

---
+++ src/main/java/hello/Servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.Servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter // getter setter 자동생성
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
